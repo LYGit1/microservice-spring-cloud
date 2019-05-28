eea6ca2da2a9fdfde5fd3993c41bdb56102fd627
LYGit1 <33824690+LYGit1@users.noreply.github.com>
2019-05-28 10:52:07 +0800

    注册eureka客户端

--- microservice-springcloud-user/src/main/java/com/tommy/cloud/microservicespringclouduser/MicroserviceSpringcloudUserApplication.java
+++ microservice-springcloud-user/src/main/java/com/tommy/cloud/microservicespringclouduser/MicroserviceSpringcloudUserApplication.java
@@ -2,8 +2,10 @@ package com.tommy.cloud.microservicespringclouduser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MicroserviceSpringcloudUserApplication {
 
     public static void main(String[] args) {
