23cc8ba34c2e5891d501b0b4912feb327f735a69
LYGit1 <33824690+LYGit1@users.noreply.github.com>
2019-05-29 16:20:12 +0800

    feign必须配合Ribbon使用

--- microservice-springcloud-movie/src/main/java/com/tommy/cloud/microservicespringcloudmovie/MicroserviceSpringcloudMovieApplication.java
+++ microservice-springcloud-movie/src/main/java/com/tommy/cloud/microservicespringcloudmovie/MicroserviceSpringcloudMovieApplication.java
@@ -2,6 +2,7 @@ package com.tommy.cloud.microservicespringcloudmovie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 public class MicroserviceSpringcloudMovieApplication {
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
