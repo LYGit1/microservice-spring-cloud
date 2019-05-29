13bf3e3a520b946467c3b7d14a627d9edb92a7c4
LYGit1 <33824690+LYGit1@users.noreply.github.com>
2019-05-29 15:11:21 +0800

    feign

--- microservice-springcloud-movie/src/main/java/com/tommy/cloud/microservicespringcloudmovie/controller/MovieController.java
+++ microservice-springcloud-movie/src/main/java/com/tommy/cloud/microservicespringcloudmovie/controller/MovieController.java
@@ -1,6 +1,7 @@
 package com.tommy.cloud.microservicespringcloudmovie.controller;
 
 import com.tommy.cloud.microservicespringcloudmovie.entity.User;
+import com.tommy.cloud.microservicespringcloudmovie.feign.UserFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,8 +13,21 @@ public class MovieController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private UserFeignClient client;
+
     @GetMapping("/movie/{id}")
     public User findUserById(@PathVariable Long id){
         return restTemplate.getForObject("http://localhost:7902/sample/"+id,User.class);
+    }
+
+    /**
+     * @description feign方式通过eureka调用microservice-springcloud-user
+     * @param id
+     * @return
+     */
+    @GetMapping("/user/{id}")
+    public User getUser(@PathVariable Long id){
+        return client.getUser(id);
     }
 }
