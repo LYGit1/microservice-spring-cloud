ea8eb7365c23502ed4b4c80deb07df04a59375dc
LYGit1 <33824690+LYGit1@users.noreply.github.com>
2019-05-30 16:51:31 +0800

    1

--- microservice-springcloud-movie-hystrix1/src/main/java/com/tommy/cloud/microservicespringcloudmoviehystrix1/MicroserviceSpringcloudMovieHystrix1Application.java
+++ microservice-springcloud-movie-hystrix1/src/main/java/com/tommy/cloud/microservicespringcloudmoviehystrix1/MicroserviceSpringcloudMovieHystrix1Application.java
@@ -3,6 +3,7 @@ package com.tommy.cloud.microservicespringcloudmoviehystrix1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +17,7 @@ import org.springframework.web.client.RestTemplate;
 public class MicroserviceSpringcloudMovieHystrix1Application {
 
     @Bean
+    @LoadBalanced
     RestTemplate getRestTemplate(){
         return new RestTemplate();
     }
