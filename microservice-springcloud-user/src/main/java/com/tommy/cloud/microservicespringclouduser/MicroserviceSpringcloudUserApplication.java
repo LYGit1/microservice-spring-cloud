b35d08946929401b0fb05862e88c2a26025116a1
LYGit1 <33824690+LYGit1@users.noreply.github.com>
2019-06-04 20:18:30 +0800

    消息总线

--- microservice-springcloud-user/src/main/java/com/tommy/cloud/microservicespringclouduser/MicroserviceSpringcloudUserApplication.java
+++ microservice-springcloud-user/src/main/java/com/tommy/cloud/microservicespringclouduser/MicroserviceSpringcloudUserApplication.java
@@ -1,12 +1,26 @@
 package com.tommy.cloud.microservicespringclouduser;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RestController
+@RefreshScope
 public class MicroserviceSpringcloudUserApplication {
+
+    @Value("${foo}")
+    String foo;
+
+    @GetMapping("/hi")
+    public String hi(){
+        return foo;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MicroserviceSpringcloudUserApplication.class, args);
